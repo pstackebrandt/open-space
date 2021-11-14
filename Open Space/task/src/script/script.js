6382a220789c7f33982d20e39e539fee80fcd001
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2021-11-14 21:51:22 +0100

    stage 3 finished Colorize control panel

--- Open Space/task/src/script/script.js
+++ Open Space/task/src/script/script.js
@@ -9,3 +9,49 @@ function checkControlPanelPosition() {
 
     alert(`controlDeckTop: ${controlDeckTop}, spaceHeight: ${spaceHeight}, controlPanelTooLow: ${controlPanelTooLow}`);
 }
+
+function authorizeControlAccess() {
+    console.log("authorizeControlAccess()")
+
+    let controlAuthorised = getAuthorizationState();
+
+    if (controlAuthorised === true) {
+        disableAuthorizers();
+        switchSecuredElements(controlAuthorised);
+    }
+}
+
+function getAuthorizationState() {
+    const passwordElement = document.getElementById("password");
+
+    let controlAuthorised = false;
+    if (passwordElement.value === "TrustNo1") {
+        controlAuthorised = true;
+        console.log("control is authorised");
+    }
+    return controlAuthorised;
+}
+
+function disableAuthorizers() {
+    let authorizerElements = document.getElementsByClassName("authorizer");
+
+    for (let i in authorizerElements) {
+        authorizerElements[i].disabled = true;
+        console.log(`disable authorizer of type ${authorizerElements[i].type}`);
+    }
+}
+
+function switchSecuredElements(controlAuthorised) {
+    let securedItems = document.getElementsByClassName("secured");
+    console.log("count of secured items: " + securedItems.length);
+
+    for (let i in securedItems) {
+        if (controlAuthorised === true) {
+            securedItems[i].disabled = false;
+            console.log(`enable element type ${securedItems[i].type}, disabled? ${securedItems[i].disabled}`);
+        } else {
+            securedItems[i].disabled = true;
+            console.log(`disable element type ${securedItems[i].type} disabled? ${securedItems[i].enabled}`);
+        }
+    }
+}
